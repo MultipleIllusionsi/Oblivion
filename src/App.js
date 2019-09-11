c15963dad10c98b215b887237ea0486cce638dff
MultipleIllusionsi <seregaserega06@gmail.com>
2019-09-12 03:26:46 +0500

    add header and registration section

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
+import Registration from "./pages/registration/registration";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={Registration} />
       </Switch>
     </div>
   );
---
+++ src/pages/registration/registration.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in";
+
+import "./registration.scss";
+
+const Registration = () => (
+  <div className="registration">
+    <SignIn />
+  </div>
+);
+
+export default Registration;
