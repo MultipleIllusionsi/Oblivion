8b5a029880377a5e28db9d95082b5959fa3c437d
MultipleIllusionsi <seregaserega06@gmail.com>
2019-09-30 22:43:07 +0500

    fix logger for dev/prod mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,15 @@ import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(
   rootReducer,
   applyMiddleware(...middlewares)
 );
 
-export const persistor = persistStore(store); //persisted version of the store
+export const persistor = persistStore(store);
