2d17248e56c44be13ce8882d0ecdfb50bac712d9
MultipleIllusionsi <seregaserega06@gmail.com>
2019-10-03 06:13:07 +0500

    implemented saga emails and google sign in w/ refactor code for this

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,10 +2,7 @@ import { takeLatest, put, all, call } from "redux-saga/effects";
 
 import UserActionTypes from "./user.types";
 
-import {
-  googleSignInSuccess,
-  googleSignInFailure
-} from "./user.actions";
+import { signInSuccess, signInFailure } from "./user.actions";
 
 import {
   auth,
@@ -13,29 +10,56 @@ import {
   createUserProfileDocument
 } from "../../firebase/firebase.utils";
 
-export function* signinWithGoogle() {
+export function* getSnapshotFromUserAuth(userAuth) {
   try {
-    const { user } = yield auth.signInWithPopup(googleProvider);
-    const userRef = yield call(createUserProfileDocument, user);
+    const userRef = yield call(createUserProfileDocument, userAuth);
     const userSnapshot = yield userRef.get();
     yield put(
-      googleSignInSuccess({
+      signInSuccess({
         id: userSnapshot.id,
-        ...userSnapshot.data
+        ...userSnapshot.data()
       })
     );
   } catch (error) {
-    yield put(googleSignInFailure(error));
+    yield put(signInFailure(error));
+  }
+}
+
+export function* signInWithGoogle() {
+  try {
+    const { user } = yield auth.signInWithPopup(googleProvider);
+    yield getSnapshotFromUserAuth(user);
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
+
+export function* signInWithEmail({ payload: { email, password } }) {
+  try {
+    const { user } = yield auth.signInWithEmailAndPassword(
+      email,
+      password
+    );
+    yield getSnapshotFromUserAuth(user);
+  } catch (error) {
+    yield put(signInFailure(error));
   }
 }
 
 export function* onGoogleSignInStart() {
   yield takeLatest(
     UserActionTypes.GOOGLE_SIGN_IN_START,
-    signinWithGoogle
+    signInWithGoogle
+  );
+}
+
+export function* onEmailSignInStart() {
+  yield takeLatest(
+    UserActionTypes.EMAIL_SIGN_IN_START,
+    signInWithEmail
   );
 }
 
 export function* userSagas() {
-  yield all([call(onGoogleSignInStart)]);
+  yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
 }
