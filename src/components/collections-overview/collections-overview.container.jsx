35ff647d04a6b21d296bb7a8a38c364f1035c920
MultipleIllusionsi <seregaserega06@gmail.com>
2019-10-08 19:40:49 +0500

    refactor cartIconContainer w/ compose func

--- src/components/collections-overview/collections-overview.container.jsx
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -24,7 +24,6 @@ const CollectionsOverviewContainer = () => (
   <Query query={GET_COLLECTIONS}>
     {/* give a func */}
     {({ loading, error, data }) => {
-      console.log("data", data);
       if (loading) return <Spinner />;
       if (error) console.log(error);
       return <CollectionsOverview collections={data.collections} />;
